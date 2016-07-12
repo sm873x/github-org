5a4c29b10813c9784e0b04f8dc50370cbed35ead
Stella Ma <sm873x@gmail.com>
2016-07-12 14:05:06 -0400

    HTML - added section to retain json string text. JS - created ajax function to pull json data from jisaacks url and added done and fail functions. At this point, html displays 'objects' in the results function

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,38 @@
-(function() {
+(function(ns) {
+    'use strict';
+
+    window.githuborg = ns = ( ns || {});
+
+    $('.load').on('click', function() {
+
+        $.ajax({
+            url: 'https://api.github.com/users/jisaacks/orgs',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            dataType: 'json',
+        })
+        .done(function(data) {
+            $('.results').text( data );
+
+            data.forEach(i)
+            console.log(data);
+        })
+        .fail(function(xhr) {
+            handleAjaxError( xhr, $('.results') );
+        });
+
+    });
+
+    function handleAjaxError(xhr) {
+        if (xhr.status === 404) {
+            $('.results').text('Nobody home');
+        } else {
+            $('.results').text('Ruh roh');
+        }
+    }
 
 
 
-
-
-    
-})();
+})(window.githuborg);
