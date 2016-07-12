23b40941e4eeb2166b235d75b6f439b37b9688b7
Stella Ma <sm873x@gmail.com>
2016-07-12 16:35:51 -0400

    SASS = inline display name of org next to avatar image. JS - fixed list issue with lastchild

--- javascript/main.js
+++ javascript/main.js
@@ -2,8 +2,25 @@
     'use strict';
 
     window.githuborg = ns = ( ns || {});
+    //
+    // var newUser;
+    //
+    // $('.userForm').on( 'submit', function(e) {
+    //     e.preventDefault();
+    //
+    //     var newInput = $('.username').val();
+    //
+    //     return newUser = changeUser(newInput);
+    //
+    //
+    // })
+    //
+    // function changeUser(newInput) {
+    //     var newURL = 'https\:api.github.com/users/' + newInput + '/orgs';
+    //     return newURL;
+    // }
 
-    $('.load').on('click', function() {
+    $('.load').on('click', function loadJSON() {
 
         $.ajax({
             url: 'https://api.github.com/users/jisaacks/orgs',
@@ -35,7 +52,7 @@
         data.forEach( function(org) {
             $('.results')
                 .append('<li class="orgs"></li>')
-                .find('li')
+                .find('li:last-child')
                     .append('<img class="avatar" src=' + org.avatar_url + '/>')
                     .append('<p class="orgName">' + org.login + '</p>');
             });
